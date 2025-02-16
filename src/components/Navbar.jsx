11932756bee323f957903e2026db8bd039d2548c
Ankit Majee <ankitmajee.05@gmail.com>
2025-02-16 23:18:41 +0530

    Fix:-Vercel Build log

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import NavbarItem from './NavbarItem';
+import NavbarItem from './Navbaritem';
 export default function Navbar() {
   return (
     <div className='flex items-center dark:bg-gray-600 bg-lime-100 p-4 lg:text-lg justify-center gap-6 mt-2 w-full'>

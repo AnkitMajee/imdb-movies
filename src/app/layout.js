b051db8a21dbf5de8393e1cc1a902d1f27cec4e0
Ankit Majee <ankitmajee.05@gmail.com>
2025-02-16 23:03:58 +0530

    Add SearchBox component and SearchPage for movie search functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import DarkTheme from "@/components/DarkTheme";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/Navbar";
+import SearchBox from "@/components/SearchBox";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
       <div>
       <Header />
       <Navbar />
+      <SearchBox />
       </div>
         {children}
       </DarkTheme>
